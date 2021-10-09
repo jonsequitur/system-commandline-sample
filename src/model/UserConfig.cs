b6d7064ea0fb0ba9c96109407197b78f983b0f57
bartr <bartr@microsoft.com>
2021-10-09 18:05:35 -0500

    added UserConfig model

---
+++ src/model/UserConfig.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace SCL
+{
+    /// <summary>
+    /// Model for commands that use --user
+    /// System.CommandLine will parse and pass to the handler
+    /// </summary>
+    public sealed class UserConfig : AppConfig
+    {
+        public string User { get; set; }
+    }
+}

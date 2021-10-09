defd2bc2c58f2c36281cad640342de929004ad71
bartr <bartr@microsoft.com>
2021-10-09 16:09:20 -0500

    added documentation

--- src/Program.cs
+++ src/Program.cs
@@ -23,30 +23,42 @@ namespace SCL
         /// <returns>0 on success</returns>
         public static int Main(string[] args)
         {
-            // run help if no params passed
+            // run help if no params passed - this prevents an error message
             if (args == null || args.Length == 0)
             {
                 args = new string[] { "--help" };
             }
 
+            // we all need ascii art :)
             DisplayAsciiArt(args);
 
             // build the command line args
             RootCommand root = BuildRootCommand();
 
-            // run the app
+            // invoke the correct command handler
+            // once you understand what this one line of code does, it's really cool!
+            // we add a command handler for each of the leaf commands and this automatically calls that handler
+            // no switch or if statements!
+            // allows for super clean code with no parsing!
             return root.Invoke(args);
         }
 
         // Build the RootCommand using System.CommandLine
         private static RootCommand BuildRootCommand()
         {
+            // this is displayed in the help message
             RootCommand root = new ()
             {
                 Name = "scl",
                 Description = "System.CommandLine Sample App",
                 TreatUnmatchedTokensAsErrors = true,
             };
+
+            // we use extensions to build each command which makes reuse and reorg really fast and easy
+            // notice there is no help or version command added
+            // --help -h -? and --version are "automatic"
+            // --version is controlled by the semver in the project
+            //   versionprefix and versionsuffix
 
             // add the command handlers
             root.AddAddCommand();
@@ -62,6 +74,8 @@ namespace SCL
             root.AddSyncCommand();
 
             // add the global options
+            // these options are available to all commands and sub commands
+            // see AddBootstrapCommand for additional options on the commands
             root.AddGlobalOption(new Option<bool>(new string[] { "--dry-run", "-d" }, "Validates and displays configuration"));
             root.AddGlobalOption(new Option<bool>(new string[] { "--verbose", "-v" }, "Show verbose output"));
 
@@ -71,42 +85,42 @@ namespace SCL
         // display Ascii Art
         private static void DisplayAsciiArt(string[] args)
         {
-            if (args != null)
+            if (args == null || args.Length == 0 ||
+                    (!args.Contains("--version") &&
+                        (args.Contains("-h") ||
+                            args.Contains("-?") ||
+                            args.Contains("--help") ||
+                            args.Contains("--dry-run"))))
             {
-                if (!args.Contains("--version") &&
-                    (args.Contains("-h") ||
-                    args.Contains("-?") ||
-                    args.Contains("--help") ||
-                    args.Contains("--dry-run")))
+                // you have to get the path for this to work as a dotnet tool
+                string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string file = $"{path}/files/ascii-art.txt";
+
+                try
                 {
-                    string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                    string file = $"{path}/files/ascii-art.txt";
-
-                    try
+                    if (File.Exists(file))
                     {
-                        if (File.Exists(file))
+                        string txt = File.ReadAllText(file);
+
+                        if (!string.IsNullOrWhiteSpace(txt))
                         {
-                            string txt = File.ReadAllText(file);
+                            txt = txt.Replace("\r", string.Empty);
+                            string[] lines = txt.Split('\n');
 
-                            if (!string.IsNullOrWhiteSpace(txt))
+                            foreach (string line in lines)
                             {
-                                txt = txt.Replace("\r", string.Empty);
-                                string[] lines = txt.Split('\n');
-
-                                foreach (string line in lines)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                                    Console.WriteLine(line);
-                                }
-
-                                Console.ResetColor();
+                                // GEAUX Tigers!
+                                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                                Console.WriteLine(line);
                             }
+
+                            Console.ResetColor();
                         }
                     }
-                    catch
-                    {
-                        // ignore any errors
-                    }
+                }
+                catch
+                {
+                    // ignore any errors
                 }
             }
         }
